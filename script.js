26e1ec834d88f93ad72a4aecece08e60c992f258
AlmogAsrass <91328988+AlmogAsrass@users.noreply.github.com~>
2023-09-14 21:12:09 +0300

    add hover effect to divs.

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ const container = document.querySelector('.container');
 
 let divs = [];
 
-for (let i=0; i<256; i++){
+for (let i = 0; i < 256; i++) {
     divs.push(document.createElement('div'));
 }
 
-divs.forEach ((div) => {
-    container.appendChild(div)
+divs.forEach((div) => {
+    container.appendChild(div);
+    div.addEventListener('mouseover', () => {
+        div.classList.toggle('pixel')
+    })
 })
+
+/*divs.forEach((div) => {
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = 'grey';
+    })
+})*/
