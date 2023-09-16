11b9c43fd138e1027b4fb301f0d12327ecd72846
AlmogAsrass <91328988+AlmogAsrass@users.noreply.github.com~>
2023-09-16 10:19:51 +0300

    Add button that asks for the number of squares per side on the grid.

--- script.js
+++ script.js
@@ -2,19 +2,37 @@ const container = document.querySelector('.container');
 
 let divs = [];
 
-for (let i = 0; i < 256; i++) {
+/*for (let i = 0; i < 256; i++) {
     divs.push(document.createElement('div'));
 }
 
 divs.forEach((div) => {
     container.appendChild(div);
     div.addEventListener('mouseover', () => {
-        div.classList.toggle('pixel')
-    })
-})
-
-/*divs.forEach((div) => {
-    div.addEventListener('mouseover', () => {
+        //div.classList.toggle('pixel')
         div.style.backgroundColor = 'grey';
     })
 })*/
+
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+    let userInput;
+    userInput = Number(prompt('Enter a Number'));
+    if (userInput > 100) {
+        userInput = Number(prompt('Enter a Number between 1 and 100'));
+    } else {
+        for (let i = 0; i < userInput * userInput; i++) {
+            divs.push(document.createElement('div'));
+        }
+
+        divs.forEach((div) => {
+            container.appendChild(div);
+            div.style.cssText = `height: ${960 / userInput}px; width: ${960 / userInput}px`;
+            div.addEventListener('mouseover', () => {
+                //div.classList.toggle('pixel')
+                div.style.backgroundColor = 'grey';
+            })
+        })
+    }
+})
